abe0cf3f62c8430fe5d743fd594095c2bf15f1d1
Brian McPhillips <B.McPhillips89@gmail.com>
2020-07-06 21:38:46 -0700

    could not get quiz button to work, will get button working when class video is posted

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ import {isCorrect} from './functions.js';
 const theButton = document.getElementById('quiz-button');
 const result = document.getElementById('results');
 // initialize state
-theButton.addEventListener('click', () => {
-    alert('welcome to my quiz!');
-    const confirmation = confirm('Do you really want to take my quiz?')
-    console.log(confirmation);
+theButton.onclick = () =>
+    let numberCorrect = 0;
+    alert('welcome to my microphone quiz!');
+    const userName = prompt('What is your name? ');
+    const quizConfirm = confirm(`Do you really want to take my quiz ${userName}?`);
+    console.log(quizConfirm);
 
-    const answer = prompt()
+    if (quizConfirm) {
+        const userAnswer1 = prompt
+    }
 }
 
 // set event listeners to update state and DOM
--- styles/functions.js
+++ styles/functions.js
@@ -1,4 +1,9 @@
 export const isCorrect = (word) => {
 const firstLetter = word.charAt(0);
+const lowerFirst = firstLetter.toLowerCase();
+if (lowerFirst === 'y') {
+    return true;
+    
+}
 
 }
